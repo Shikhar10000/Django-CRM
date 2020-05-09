b084410d8573e3337b399a2e4db3852db416843e
Shikhar Maheshwari <37925362+Shikhar10000@users.noreply.github.com>
2020-05-10 00:33:42 +0530

    removed whitenoise

--- crm/wsgi.py
+++ crm/wsgi.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 PROJECT_DIR = os.path.abspath(__file__)
 sys.path.append(PROJECT_DIR)
